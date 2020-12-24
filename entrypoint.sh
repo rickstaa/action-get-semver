060cf3857ee4a1119f0ed1fa3c6853ffc3e209a3
rickstaa <rick.staa@outlook.com>
2020-12-24 17:22:58 +0100

    :bug: Fixes current version bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-input_bump_level="$(echo ${INPUT_BUMP_LEVEL} | tr '[:upper:]' '[:lower:]')" # Make lowercase
+input_bump_level="$(echo ${INPUT_BUMP_LEVEL:-patch} | tr '[:upper:]' '[:lower:]')" # Make lowercase
 case $input_bump_level in
   "major"|"minor"|"patch")
     ;;
@@ -20,6 +20,26 @@ git fetch --tags # Fetch existing tags before bump.
 git fetch --prune --unshallow
 CURRENT_VERSION="$(bump current)" || true
 NEXT_VERSION="$(bump ${input_bump_level})" || true
+
+# Set next version tag in case existing tags not found.
+if [ -z "${NEXT_VERSION}" ] && [ -z "$(git tag)" ]; then
+	case "${input_bump_level}" in
+		major)
+			NEXT_VERSION="v1.0.0"
+			;;
+		minor)
+			NEXT_VERSION="v0.1.0"
+			;;
+		patch)
+			NEXT_VERSION="v0.0.1"
+			;;
+	esac
+fi
+
+if [ -z "${NEXT_VERSION}" ]; then
+  echo "Cannot find next version."
+  exit 1
+fi
 
 echo "::set-output name=current_version::${CURRENT_VERSION}"
 echo "::set-output name=next_version::${NEXT_VERSION}"

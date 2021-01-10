4da04891ea8011bd46368cac7eef2c3a2113e874
rickstaa <rick.staa@outlook.com>
2021-01-10 16:59:58 +0100

    :sparkles: Adds frail input argument and error output argument
    This commit gives the user the ability to let the action fail when the
    current version is not found. This can be done through the `frail` input
    argument. When frail is set to 'false' still sets the `error` output
    argumen to true to let the user know that an error occured.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,40 +2,49 @@
 set -e
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
-  cd "${GITHUB_WORKSPACE}" || exit
+	cd "${GITHUB_WORKSPACE}" || exit
 fi
 
 input_bump_level="$(echo ${INPUT_BUMP_LEVEL:-patch} | tr '[:upper:]' '[:lower:]')" # Make lowercase
 case $input_bump_level in
-  "major"|"minor"|"patch")
-    ;;
-  *)
-    printf '%s\n' "Please specify a valid bump level \`${input_bump_level}\` is not valid [major, minor, patch]."
-    exit 1
-    ;;
+"major" | "minor" | "patch") ;;
+
+*)
+	printf '%s\n' "[action-get-semver] Please specify a valid bump level \`${input_bump_level}\` is not valid [major, minor, patch]."
+	exit 1
+	;;
 esac
 
 CURRENT_VERSION="$(bump current)" || true
 NEXT_VERSION="$(bump ${input_bump_level})" || true
 
-# Set next version tag in case existing tags not found.
-if [ -z "${NEXT_VERSION}" ] && [ -z "$(git tag)" ]; then
+if [[ -z "${CURRENT_VERSION}" ]]; then
+	echo "[action-get-semver] Current version tag not found."
+	echo "::set-output name=error::true"
+	if [[ "${INPUT_FRAIL}" = 'true' ]]; then
+		exit 1
+	fi
+
+	# Create current and next tag
+	CURRENT_VERSION="v0.0.0"
 	case "${input_bump_level}" in
-		major)
-			NEXT_VERSION="v1.0.0"
-			;;
-		minor)
-			NEXT_VERSION="v0.1.0"
-			;;
-		patch)
-			NEXT_VERSION="v0.0.1"
-			;;
+	major)
+		NEXT_VERSION="v1.0.0"
+		;;
+	minor)
+		NEXT_VERSION="v0.1.0"
+		;;
+	patch)
+		NEXT_VERSION="v0.0.1"
+		;;
 	esac
+else
+	echo "::set-output name=error::false"
 fi
 
 if [ -z "${NEXT_VERSION}" ]; then
-  echo "Cannot find next version."
-  exit 1
+	echo "Cannot find next version."
+	exit 1
 fi
 
 if [ "${INPUT_VERBOSE}" == "true" ]; then
